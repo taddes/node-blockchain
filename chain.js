5175072e9f3433e297d6cbdce38521a05446ffd7
Taddes Korris <taddes.korris@passportinc.com>
2019-08-18 16:02:48 -0400

    blockchain class

--- chain.js
+++ chain.js
@@ -1,3 +1,80 @@
 const Block = require('./block');
 const actions = require('./constants');
 const { generateProof, isProofValid } = require('../utils/proof');
+
+class Blockchain {
+  constructor(blocks, io) {
+    this.blocks = blocks || [new Block(0, 1, 0, [])];
+    this.currentTransactions = [];
+    this.nodes = [];
+    this.io = io;
+  }
+
+  addNode(node) {
+    this.nodes.push(node)
+  }
+
+  mineBlock(block) {
+    this.blocks.push(block);
+    console.log('Block Mined Successfully');
+    this.io.emit(actions.END_MINING, this.toArray());
+  }
+
+  async newTransaction(transaction) {
+    this.currentTransactions.push(transaction);
+    if (this.currentTransactions.length === 2) {
+      console.log('Initiating mining of block....');
+      const previousBlock = this.lastBlock();
+      process.env.BREAK = false;
+      const block = new Block(previousBlock.getIndex() + 1, previousBlock.hashValue(), previousBlock.getProof(), this.currentTransactions);
+      const { proof, dontMine} = await generateProof(previousBlock.getProof());
+      block.setProof(proof);
+      this.currentTransactions = [];
+      if (dontMine !== 'true') {
+        this.mineBlock(block);
+      }
+    }
+  }
+
+  lastBlock() {
+    return this.blocks[this.blocks.length - 1];
+  }
+
+  getLength() {
+    return this.blocks.length;
+  }
+
+  checkValidity() {
+    const { blocks } = this;
+    let previousBlock = blocks[0];
+    for (let i = 1; i < blocks.length; i++) {
+      const currentBlock = blocks[i];
+      if (currentBlock.getPreviousBlockHash() !== previousBlock.hashValue()) {
+        return false;
+      }
+      if (!isProofValid(previousBlock.getProof(), currentBlock.getProof())) {
+        return false
+      }
+      previousBlock = currentBlock;
+    }
+    return true
+  }
+
+  praseChain(blocks) {
+    this.blocks = blocks.map(block => {
+      const parsedBlock = new Block(0);
+      parsedBlock.parseBlock(block);
+      return parsedBlock;
+    });
+  }
+  
+  toArray() {
+    return this.blocks.map(block => block.getDetails());
+  }
+
+  printBlocks() {
+    this.blocks.forEach(block => console.log({block}))
+  }
+}
+
+ module.exports = Blockchain;
