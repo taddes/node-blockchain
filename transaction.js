367800befd5d58d5d379b8e1d2246241bb3e5b4f
Taddes Korris <taddes.korris@passportinc.com>
2019-08-18 15:42:48 -0400

    transaction revisions

--- transaction.js
+++ transaction.js
@@ -5,6 +5,23 @@ class Transaction {
     this.amount = amount;
     this.timestamp = Date.now();
   }
+
+  getDetails() {
+    const { sender, receiver, amount, timestamp } = this;
+    return {
+      sender,
+      receiver,
+      amount,
+      timestamp
+    };
+  }
+
+  parseTransaction(transaction) {
+    this.sender = transaction.sender;
+    this.receiver = transaction.receiver;
+    this.amount = transaction.amount;
+    this.timestamp = transaction.timestamp;
+  }
 }
 
 module.exports = Transaction;
